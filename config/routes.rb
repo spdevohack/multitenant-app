42bc88ee03178d5edce61959f0776b4c59459e25
sachin <sachin@devohack.com>
2022-03-07 15:07:44 +0530

    Complete Multitenant

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+class SubdomainConstraint 
+  def self.matches?(request)
+    subdomains = %w{ www admin }
+    request.subdomain.present? && !subdomains.include?(request.subdomain)
+  end
+end
+
+Rails.application.routes.draw do
+  constraints SubdomainConstraint  do 
+    resources :projects
+  end
+
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
