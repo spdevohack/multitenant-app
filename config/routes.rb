1e8d4fabcf9fd86761192cacd994a52953ed6f37
sachin <sachin@devohack.com>
2022-03-07 15:20:20 +0530

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
